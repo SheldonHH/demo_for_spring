2261a11eb8f28663a63fac4d743523d9b6f8d439
bosun-further <constantgrower@gmail.com>
2022-05-03 16:39:10 +0800

    vi HMatrix successful

--- src/main/java/com/example/demo/model/RCVisTuple.java
+++ src/main/java/com/example/demo/model/RCVisTuple.java
@@ -2,28 +2,30 @@ package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
+
 // response to server's request of the specified row and col number
 public class RCVisTuple {
-    public String[][] row_vi;
-    public String[][] col_vi;
-    public RCVisTuple(@JsonProperty("row_vi") String[][] row_vi, @JsonProperty("col_vi")  String[][] col_vi) {
+    public ArrayList<String> row_vi;
+    public ArrayList<String> col_vi;
+    public RCVisTuple(@JsonProperty("row_vi") ArrayList<String> row_vi, @JsonProperty("col_vi")  ArrayList<String> col_vi) {
         this.row_vi = row_vi;
         this.col_vi = col_vi;
     }
 
-    public void setRow_vi(String[][] row_vi) {
+    public void setRow_vi(ArrayList<String> row_vi) {
         this.row_vi = row_vi;
     }
 
-    public void setCol_vi(String[][] col_vi) {
+    public void setCol_vi(ArrayList<String> col_vi) {
         this.col_vi = col_vi;
     }
 
-    public String[][] getRow_vi() {
+    public ArrayList<String> getRow_vi() {
         return row_vi;
     }
 
-    public String[][] getCol_vi() {
+    public ArrayList<String> getCol_vi() {
         return col_vi;
     }
 }
